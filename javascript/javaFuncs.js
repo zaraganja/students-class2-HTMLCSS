1202e9eae14c16d87be14975ed00e9eb40917d98
zaraganja <sahba.ganji@gmail.com>
2025-01-22 21:32:38 +0330

    start js

---
+++ javascript/javaFuncs.js
@@ -0,0 +1,21 @@
+// ES6
+// اسم یک تابع باید یونیک باشه 
+// arrow function 
+const alertMyMsg =()=>{
+    alert("hi Java Script")
+    console.log("hi") //error handling and test project
+}
+
+// traditional functions
+// function alertMyMsg(){
+
+// }
+
+const writeHello=()=>{
+    alert("hello js!");
+    document.getElementById("emptySpace").innerHTML="hello students";
+    document.getElementById("emptySpace").style.color="red";
+    document.getElementById("emptySpace").style.textDecoration="line-through"
+}
+
+// تعریف متغیر 
